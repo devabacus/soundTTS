bdc76d2f007b418d73cc2f672b1b3409379b1eb3
devabacus <devabacusnrf@gmail.com>
2023-05-18 19:47:25 +0530

    word

--- __init__.py
+++ __init__.py
@@ -7,11 +7,13 @@ import requests
 import shutil
 import time
 import os
+from functools import partial
+import re
 
-progress_dialog = None
+def clean_filename(filename):
+    return re.sub(r'[\\/*?:"<>|]', "", filename)
 
-def download_sound(text, language, output_folder, progress_callback):
-    # Step 1: Send a POST request to the API to request the sound file
+def download_sound(text, language, output_folder):
     url = "https://api.soundoftext.com/sounds"
     data = {
         "engine": "Google",
@@ -28,7 +30,6 @@ def download_sound(text, language, output_folder, progress_callback):
         print("Error occurred: ", response_json["message"])
         return None
 
-    # Step 2: Retrieve the sound file using the sound ID
     sound_id = response_json["id"]
     sound_url = f"https://api.soundoftext.com/sounds/{sound_id}"
 
@@ -45,57 +46,46 @@ def download_sound(text, language, output_folder, progress_callback):
 
         time.sleep(2)
 
-    # Step 3: Download the sound file and save it locally
     response = requests.get(download_url, stream=True)
-    filename = f"{text[:30].replace(' ', '_').replace('?', '')}.mp3"
+    filename = clean_filename(f"{text[:30].replace(' ', '_')}") + '.mp3'
     file_path = f"{output_folder}/{filename}"
 
     with open(file_path, "wb") as sound_file:
         shutil.copyfileobj(response.raw, sound_file)
 
-    # Notify about the progress
-    progress_callback()
-
     return filename
-
 
 def on_edit_action(browser):
     selected_cards = browser.selectedCards()
+    
+    progress_dialog = QProgressDialog("Processing cards...", "", 0, len(selected_cards), browser)
+    progress_dialog.setWindowModality(Qt.WindowModal)
+    progress_dialog.setWindowTitle("Please Wait")
+    progress_dialog.setCancelButton(None)  # Disallow canceling
+    progress_dialog.setMinimumDuration(0)
+    progress_dialog.show()  # Make dialog visible immediately
 
-    # Choose your language and output_folder accordingly
     output_folder = os.path.join(mw.pm.profileFolder(), "collection.media")
     language = "en-US"
 
-    
-    
     for i, card_id in enumerate(selected_cards):
         card = mw.col.getCard(card_id)
         note = card.note()
-        front_text = note.fields[0]  # Assuming Front is the first field
+        front_text = note.fields[0]
 
-        def progress_callback():
-            progress_dialog.setValue(i + 1)
-            QCoreApplication.processEvents()  # Update the UI
-
-        filename = download_sound(front_text, language, output_folder, progress_callback)
+        filename = download_sound(front_text, language, output_folder)
 
         if filename:
-            # Generate audio tag for the mp3 file
             audio_tag = f"[sound:{filename}]"
-            # Append the audio tag to the Front field
             note.fields[0] += audio_tag
             note.flush()
-        else:
-            showInfo(f"Could not download sound file for card {card_id}.")
 
-    if progress_dialog:
-        while progress_dialog.value() < progress_dialog.maximum():
-            QCoreApplication.processEvents()  # Update the UI
+        progress_dialog.setValue(i+1)
+        QCoreApplication.processEvents()  # Update the UI
 
 def setup_menu_item(browser):
     action = QAction("Add TTS to Front", browser)
-    action.triggered.connect(lambda _, b=browser: on_edit_action(b))
+    action.triggered.connect(partial(on_edit_action, browser))
     browser.form.menuEdit.addAction(action)
-    
 
 addHook("browser.setupMenus", setup_menu_item)
