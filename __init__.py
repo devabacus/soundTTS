bc3e8c99cad9888ec6f7a75acd0784c6424c4e0a
devabacus <devabacusnrf@gmail.com>
2023-05-18 16:52:12 +0530

    1

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
-from aqt import mw
-from aqt.utils import showInfo
+from aqt import mw, dialogs
+from aqt.utils import showInfo, askUser, showText, tooltip
 from anki.hooks import addHook
-from aqt.qt import QAction
+from aqt.qt import QAction, QProgressDialog, QCoreApplication
+from PyQt6.QtCore import Qt
 import requests
 import shutil
 import time
 import os
 
-def download_sound(text, language, output_folder):
+progress_dialog = None
+
+def download_sound(text, language, output_folder, progress_callback):
     # Step 1: Send a POST request to the API to request the sound file
     url = "https://api.soundoftext.com/sounds"
     data = {
@@ -50,7 +53,11 @@ def download_sound(text, language, output_folder):
     with open(file_path, "wb") as sound_file:
         shutil.copyfileobj(response.raw, sound_file)
 
+    # Notify about the progress
+    progress_callback()
+
     return filename
+
 
 def on_edit_action(browser):
     selected_cards = browser.selectedCards()
@@ -59,12 +66,18 @@ def on_edit_action(browser):
     output_folder = os.path.join(mw.pm.profileFolder(), "collection.media")
     language = "en-US"
 
-    for card_id in selected_cards:
+    
+    
+    for i, card_id in enumerate(selected_cards):
         card = mw.col.getCard(card_id)
         note = card.note()
         front_text = note.fields[0]  # Assuming Front is the first field
 
-        filename = download_sound(front_text, language, output_folder)
+        def progress_callback():
+            progress_dialog.setValue(i + 1)
+            QCoreApplication.processEvents()  # Update the UI
+
+        filename = download_sound(front_text, language, output_folder, progress_callback)
 
         if filename:
             # Generate audio tag for the mp3 file
@@ -75,9 +88,14 @@ def on_edit_action(browser):
         else:
             showInfo(f"Could not download sound file for card {card_id}.")
 
+    if progress_dialog:
+        while progress_dialog.value() < progress_dialog.maximum():
+            QCoreApplication.processEvents()  # Update the UI
+
 def setup_menu_item(browser):
     action = QAction("Add TTS to Front", browser)
     action.triggered.connect(lambda _, b=browser: on_edit_action(b))
     browser.form.menuEdit.addAction(action)
+    
 
 addHook("browser.setupMenus", setup_menu_item)
